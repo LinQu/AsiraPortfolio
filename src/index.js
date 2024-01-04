4de5dee51a6c261f410e6c3b297fe7e64c209935
LinQu <asirayudha11@gmail.com>
2024-01-04 08:37:29 +0700

    setup gh-pages4

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-      <App />
-    </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
